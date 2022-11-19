eda17d15c60507d5417d23ee239d4fc24912366e
Arbaz Shaikh <--edit>
2022-11-19 19:28:55 +0530

    updating Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -2,5 +2,6 @@ class Multiply{
  public static void main(String args[]){
    int a = 4;
    int b= 6;
+   int c= 4;
        
-  System.out.println("The product is:"+ 
+  System.out.println("The product is:"+ a*b*c);
